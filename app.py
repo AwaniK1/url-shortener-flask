c9ddd7c74bdeed6784384ff572f3d25cf43c3996
Awani K <awani.18159@sakec.ac.in>
2026-04-17 01:25:52 +0530

    Added URL shortener logic

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, request
 
+import string
+import random
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         url = request.form['original_url']
-        return f'original url: {url}'
+        short_code = generate_short_code()
+        return f'original url: {url} | short code: {short_code}'
+        
+    else:
+        return render_template('index.html')
 
-    return render_template('index.html')
+def generate_short_code():
+    characters = string.ascii_letters + string.digits
+    code = ''
+    for i in range(6):
+        code += random.choice(characters)
+    return code
 
 if __name__ == '__main__':
-    app.run(debug='True')
+    app.run(debug=True)
